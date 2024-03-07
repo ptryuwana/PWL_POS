f86addc16e765d58424c52043711ded88e3110d1
Putra Nindya Yuwana <137630717+ptryuwana@users.noreply.github.com>
2024-03-08 00:08:42 +0700

    Praktikum 2.4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -9,27 +9,27 @@
 <body>
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
+        {{-- <tr>
             <th>Jumlah Pengguna</th>
         </tr>
         <tr>
             <th>{{ $usercount }}</th>
-        </tr>
+        </tr> --}}
         
-        {{-- <tr>
+        <tr>
             <th>ID</th>
             <th>Username</th>
             <th>Nama</th>
             <th>ID Level Pengguna</th>
         </tr>
-        @foreach ($data as $d)
+        {{-- @foreach ($data as $d) --}}
         <tr>
             <td>{{ $data->user_id }}</td>
             <td>{{ $data->username }}</td>
             <td>{{ $data->nama }}</td>
             <td>{{ $data->level_id }}</td>
         </tr>
-        @endforeach --}}
+        {{-- @endforeach --}}
     </table>
 </body>
 </html>
