e84f5f32e650ca4ddd41d0483d301589cdd06eb4
Putra Nindya Yuwana <137630717+ptryuwana@users.noreply.github.com>
2024-04-26 09:49:18 +0700

    Praktikum 1 - JS10

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,11 +11,19 @@ class UserModel extends User
 {
     use HasFactory;
 
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
+
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
     
-    public function level(): BelongsTo{
+        public function level(): BelongsTo{
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 }
