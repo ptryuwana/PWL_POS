e99f8753f4258b798d3942295774145f1794dd4d
Putra Nindya Yuwana <137630717+ptryuwana@users.noreply.github.com>
2024-05-02 22:37:33 +0700

    JS10 P4

--- routes/api.php
+++ routes/api.php
@@ -21,16 +21,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
-// Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 Route::middleware('auth:api')->get('/users', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('levels', [LevelController::class, 'index']);
-// Route::post('levels', [LevelController::class, 'store']);
-// Route::get('levels/{level}', [LevelController::class, 'show']);
-// Route::put('levels/{level}', [LevelController::class, 'update']);
-// Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+Route::get('levels', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
 
 // Route::get('user', [UserController::class, 'index']);
 // Route::post('user', [UserController::class, 'store']);
