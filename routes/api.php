32b6050186454b7d84aabdfcb04608d249c18558
Putra Nindya Yuwana <137630717+ptryuwana@users.noreply.github.com>
2024-05-02 22:08:19 +0700

    JS10 P2

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\BarangController;
+use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\LevelController;
+use App\Http\Controllers\Api\TransaksiController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +20,40 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+// Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+Route::middleware('auth:api')->get('/users', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('levels', [LevelController::class, 'index']);
+// Route::post('levels', [LevelController::class, 'store']);
+// Route::get('levels/{level}', [LevelController::class, 'show']);
+// Route::put('levels/{level}', [LevelController::class, 'update']);
+// Route::delete('levels/{level}', [LevelController::class, 'destroy']);
+
+// Route::get('user', [UserController::class, 'index']);
+// Route::post('user', [UserController::class, 'store']);
+// Route::get('user/{user}', [UserController::class, 'show']);
+// Route::put('user/{user}', [UserController::class, 'update']);
+// Route::delete('user/{user}', [UserController::class, 'destroy']);
+
+// Route::get('kategori', [KategoriController::class, 'index']);
+// Route::post('kategori', [KategoriController::class, 'store']);
+// Route::get('kategori/{kategori}', [KategoriController::class, 'show']);
+// Route::put('kategori/{kategori}', [KategoriController::class, 'update']);
+// Route::delete('kategori/{kategori}', [KategoriController::class, 'destroy']);
+
+// Route::get('barang', [BarangController::class, 'index']);
+// Route::post('barang', [BarangController::class, 'store']);
+// Route::get('barang/{barang}', [BarangController::class, 'show']);
+// Route::put('barang/{barang}', [BarangController::class, 'update']);
+// Route::delete('barang/{barang}', [BarangController::class, 'destroy']);
+
+// Route::get('transaksi', [TransaksiController::class, 'index']);
+// Route::post('transaksi', [TransaksiController::class, 'store']);
+// Route::get('transaksi/{transaksi}', [TransaksiController::class, 'show']);
+// Route::put('transaksi/{transaksi}', [TransaksiController::class, 'update']);
+// Route::delete('transaksi/{transaksi}', [TransaksiController::class, 'destroy']);
+
+// Route::post('/register1', App\Http\Controllers\Api\RegisterController::class)->name('register1');
